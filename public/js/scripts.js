ada6a9406980eb7100c027e615f61417ca25bb51
ameyashrikant <76768424+ameyashrikant@users.noreply.github.com>
2024-04-25 18:49:29 +0530

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,6 +3,11 @@ document.getElementById('runButton').addEventListener('click', function() {
         .then(response => response.text())
         .then(message => alert(message))
         .catch(error => console.error('Error:', error));
+    // fetch('/runcode').requestPictureInPicture().catch(err => {
+    //     console.log(err);
+    // }
+});
+document.getElementById('runButton').addEventListener('click', function() {
     fetch('/runcode').requestPictureInPicture().catch(err => {
         console.log(err);
     }
